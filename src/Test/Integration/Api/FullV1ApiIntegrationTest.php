0b4a716856c1dca033fb49298cf7aa22d041fb0d
Matthias Walter <matthias.walter@mwltr.de>
2018-09-17 15:44:36 +0200

    update FullApiIntegrationTest to test Item GET and use sandbox project/translate

--- src/Test/Integration/Api/FullV1ApiIntegrationTest.php
+++ src/Test/Integration/Api/FullV1ApiIntegrationTest.php
@@ -15,18 +15,26 @@ use Eurotext\RestApiClient\Enum\ProjectTypeEnum;
 use Eurotext\RestApiClient\Request\Data\Project\ItemData;
 use Eurotext\RestApiClient\Request\Data\ProjectData;
 use Eurotext\RestApiClient\Request\Project\ItemDataRequest;
+use Eurotext\RestApiClient\Request\Project\ItemGetRequest;
 use Eurotext\RestApiClient\Request\ProjectDataRequest;
 use PHPUnit\Framework\TestCase;
 
 class FullV1ApiIntegrationTest extends TestCase
 {
+    const PROJECT_DESCRIPTION    = 'Integration Test';
+    const DESCRIPTION_TRANSLATED = '!em etalsnart esaelP';
+    const DESCRIPTION            = 'Please translate me!';
+
     /** @var ProjectV1Api */
     private $projectV1Api;
 
     /** @var ItemV1Api */
     private $projectItemV1Api;
 
-    const PROJECT_DESCRIPTION = 'Integration Test';
+    private $metaData = [
+        'item_id'   => 27,
+        'more_meta' => 'eurotext are the best',
+    ];
 
     protected function setUp()
     {
@@ -41,6 +49,7 @@ class FullV1ApiIntegrationTest extends TestCase
 
     /**
      * @throws \Eurotext\RestApiClient\Exception\DeserializationFailedException
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function testItShouldCreateProject()
     {
@@ -67,19 +76,28 @@ class FullV1ApiIntegrationTest extends TestCase
             'product',
             'Magento',
             new ItemData(
-                [
-                    'description' => 'Please translate me!',
-                ],
-                [
-                    'item_id'   => 27,
-                    'more_meta' => 'eurotext are the best',
-                ]
+                ['description' => self::DESCRIPTION],
+                $this->metaData
             )
         );
 
         $response = $this->projectItemV1Api->post($itemRequest);
 
-        $this->assertGreaterThan(0, $response->getId());
+        $itemId = $response->getId();
+        $this->assertGreaterThan(0, $itemId);
+
+        return ['project_id' => $projectId, 'item_id' => $itemId];
+    }
+
+    /**
+     * @depends testItShouldCreateProject
+     */
+    public function testItShouldTriggerTranslateInSandbox(int $projectId)
+    {
+        $result = $this->projectV1Api->translate($projectId);
+
+        $this->assertGreaterThan(0, $result->id);
+        $this->assertEquals($projectId, $result->id);
     }
 
     /**
@@ -91,12 +109,9 @@ class FullV1ApiIntegrationTest extends TestCase
 
         $actualItem   = $response->getItems()[1];
         $expectedItem = [
-            'original_string' => 'Please translate me!',
-            '__meta'          => [
-                'item_id'   => 27,
-                'more_meta' => 'eurotext are the best',
-            ],
-            'status'          => '',
+            'description' => self::DESCRIPTION_TRANSLATED,
+            '__meta'      => $this->metaData,
+            'status'      => '',
         ];
 
         $this->assertSame($response->getDescription(), self::PROJECT_DESCRIPTION);
@@ -107,11 +122,19 @@ class FullV1ApiIntegrationTest extends TestCase
     }
 
     /**
-     * @depends testItShouldCreateProject
+     * @depends testItShouldCreateItem
      */
-    public function testItShouldGetItemData(int $projectId)
+    public function testItShouldGetItemData(array $data)
     {
-        $this->assertTrue(true);
-        $this->markTestIncomplete('Implement, when get is implemented');
+        $projectId = $data['project_id'];
+        $itemId    = $data['item_id'];
+        $request   = new ItemGetRequest($projectId, $itemId);
+        $response  = $this->projectItemV1Api->get($request);
+
+        $itemData = $response->getItemData();
+
+        $this->assertEquals(self::DESCRIPTION_TRANSLATED, $itemData->getDataValue('description'));
+
+        $this->assertEquals($this->metaData, $itemData->getMeta());
     }
 }
