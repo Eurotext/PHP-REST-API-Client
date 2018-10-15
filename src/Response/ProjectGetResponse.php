cb9d173cdc0972cb185608c8166c61720ea5bd02
Matthias Walter <matthias.walter@mwltr.de>
2018-10-15 11:09:43 +0200

    ETM-25 increase error output, fix status mapping

--- src/Response/ProjectGetResponse.php
+++ src/Response/ProjectGetResponse.php
@@ -21,11 +21,34 @@ class ProjectGetResponse extends AbstractResponse
     /** @var mixed[] */
     private $files;
 
-    public function __construct(string $description, array $items, array $files)
+    /**
+     * @param string $description
+     * @param ItemData[] $items
+     * @param mixed[] $files
+     */
+    public function __construct(string $description, array $items, array $files = [])
     {
+        $this->initItems($items);
         $this->description = $description;
-        $this->items       = $items;
         $this->files       = $files;
+    }
+
+    private function initItems(array $items)
+    {
+        foreach ($items as $key => $itemData) {
+            if ($itemData instanceof ItemData) {
+                $this->items[$key] = $itemData;
+                continue;
+            }
+
+            $meta = [];
+            if (array_key_exists('__meta', $itemData)) {
+                $meta = $itemData['__meta'];
+                unset($itemData['__meta']);
+            }
+
+            $this->items[$key] = new ItemData($itemData, $meta);
+        }
     }
 
     /**
