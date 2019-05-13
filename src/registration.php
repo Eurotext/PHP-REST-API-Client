47a275e2a802955ed0cc05c309e3248991cc8c79
Matthias Walter <matthias.walter@mwltr.de>
2019-05-13 09:08:04 +0200

    add registration.php

---
+++ src/registration.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::LIBRARY, 'Eurotext_RestApiClient', __DIR__);
